ca66365cfefdc4f5355d52707a549d7084ac82df
Brett <brett@applogix.com>
2016-05-26 14:24:09 -0700

    neutralise conflicts

---
+++ client/components/listing.js
@@ -0,0 +1,7 @@
+//Basic Stateless Functional Component
+var Listing = (props) => //Consider ES6 object destructuring to filter props object for desired properties only?
+  <p>{props.listing.date}</p>
+  <p>{props.listing.title}</p>
+  <p>{props.listing.description}</p>
+  <p>{props.listing.location}</p>
+  <p>{props.listing.price}</p>;
--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-//var db = require('./db');
 
 // Middleware
 var morgan = require('morgan');
