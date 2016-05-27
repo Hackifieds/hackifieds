380467062e0779c6356fb69907552d85ccf8e6ab
Brett <brett@applogix.com>
2016-05-26 20:37:18 -0700

    Server picking up client GET requests; then crashes server

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,25 +1,38 @@
-
-let helpers = {
-  ajaxGet: () => {
-    $.ajax({
-      url: '',
-      method: 'GET',
-      success: function(data) {
-        console.log('Success', data);
-        window.listing = JSON.parse(data);
-      }
-    });
-  },
-  ajaxPost: (data) => {
-    $.ajax({
-      url: '',
-      method: 'POST',
-      data: JSON.stringify(data),
-      success: function(data) {
-        console.log('Success', data);
-      }
-    });
-  }
+var ajaxGet = () => {
+  console.log('called');
+  $.ajax({
+    hostname: 'http://localhost:3000/client',
+    url: '/api/listings',
+    method: 'GET',
+    success: function(data) {
+      console.log('GET success', data);
+    },
+    error: function(a, b, c, d) {
+      console.log(a);
+      console.log(b);
+      console.log(c);
+      console.log(d);
+    }
+  });
 };
 
-module.exports = helpers;
+var ajaxPost = () => {
+  //placeholder - will be an input to the ajax request
+
+  let data = {
+    date: '26 May 2016' + Math.floor(Math.random() * 100),
+    title: 'Room to let in apartment centrally located',
+    description: 'Clean room located in heart of Tenderloin, 10 minute walk to HR',
+    location: 'Tenderloin',
+    price: '$' + Math.floor(Math.random() * 2000) + 'pm'
+  };
+
+  $.ajax({
+    url: '/api/listings',
+    method: 'POST',
+    data: JSON.stringify(data),
+    success: function(data) {
+      console.log('POST success', data);
+    }
+  });
+};
