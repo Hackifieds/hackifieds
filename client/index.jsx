de53a5ca077fbd46a2d61cf837991bbcb07e71f9
gfncodes <gfcodes@123mail.org>
2016-05-30 12:52:14 -0700

    Refactor index.jsx to render Listings component
    - Currently index.jsx renders a Listing (a single listing).  Make it
      renders Listings (a component comprised of an array of Listing
      components).
    - Also add mock data to index.jsx (an array of two sample listings).

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,25 @@
-import Listing from './components/listing.jsx';
+import Listings from './components/listings.jsx';
 
 // let ajax = require('./lib/helpers.js');
-let listing = {
-  date: '26 May 2016',
-  title: 'Room to let in apartment centrally located',
-  description: 'Clean room located in heart of Tenderloin, 10 minute walk to HR',
-  location: 'Tenderloin',
-  price: '$1,500pm'
-};
+let listingArr = [
+  {
+    date: '26 May 2016',
+    title: 'Room to let in apartment centrally located',
+    description: 'Clean room located in heart of Tenderloin, 10 minute walk to HR',
+    location: 'Tenderloin',
+    price: '$1,500pm'
+  },
+  {
+    date: '30 May 2016',
+    title: 'Room to let in Russian Hill',
+    description: 'Nice quiet neighborhood, 25 min walk to Hack Reactor',
+    location: 'Russian Hill',
+    price: '$1,400'
+  }
+];
 
 ReactDOM.render(
-  <Listing listing={listing}/>,
+  <Listings listings={listingArr}/>,
   document.getElementById('app')
 );
+
