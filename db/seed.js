1088695f2fe91a4b9ca08fe40aaed131aef97d46
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-06-01 16:07:59 -0700

    Add comments to seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,13 @@
+// To run seed.js
+// Prereq: must clear categories table before running to avoid duplicate categories (same can be applied to users and listings table if your database already has the seed data)
+// In terminal run: node seed.js
+
 var data = require('./data');
 var db = require('./db');
 
+// iterate through data.json
 data.forEach(function(table) {
+  // iterate through categories and insert into categories table
   if (table.hasOwnProperty('categories')) {
     table.categories.forEach(function(entry) {
       db.Category.create(entry)
@@ -12,6 +18,7 @@ data.forEach(function(table) {
           console.error(error);
         });
     });
+  // iterate through users and insert into users table  
   } else if (table.hasOwnProperty('users')) {
     table.users.forEach(function(entry) {
       db.User.create(entry)
@@ -22,6 +29,7 @@ data.forEach(function(table) {
           console.error(error);
         });
     });
+  // iterate through listings and insert into listings table  
   } else if (table.hasOwnProperty('listings')) {
     table.listings.forEach(function(entry) {
       db.Listing.create(entry)
