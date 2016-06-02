36cad65abe4cb4213e0a16609fd150b3861aac89
Brett <brett@applogix.com>
2016-06-02 11:04:36 -0700

    Fix merge conflicts in App.jsx

--- client/components/listingInfo.jsx
+++ client/components/listingInfo.jsx
@@ -1,10 +1,12 @@
 let ListingDetail = props => (
   <div>
+    <div class="listing-detail-date"> {props.listing.createdAt} | </div>
     <div class="listing-detail-title"> {props.listing.title} </div>
     <div class="listing-detail-location"> {props.listing.location} | </div>
     <div class="listing-detail-price"> {props.listing.price} | </div>
-    <div class="listing-detail-date"> {props.listing.createdAt} | </div>
-    <div class="listing-detail-description" listing={props.listing}> {props.listing.description}/> | </div>
+    <div class="listing-detail-description"> {props.listing.description} | </div>
+    <div class="listing-detail-email"> {props.listing.email} | </div>
+    <div class="listing-detail-telphone"> {props.listing.telephone} | </div>
   </div>
 );
 
