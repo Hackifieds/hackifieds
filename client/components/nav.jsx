9fd7838b8bdd8738386963943e1a270bd42737e2
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2016-06-01 17:14:51 -0700

    Add bootstrap and serve static files for bootstrap, style nav bar

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -2,7 +2,7 @@ let Nav = ({handleNavClick}) => (
 
   <div className="nav">
     <nav>
-      <ul>
+      <ul className="nav nav-pills nav-justified">
         <li><a onClick={() => handleNavClick('Rent')}>Rent</a></li>
         <li><a onClick={() => handleNavClick('Buy')}>Buy</a></li>
         <li><a onClick={() => handleNavClick('Hack')}>Hack</a></li>
